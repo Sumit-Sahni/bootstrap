46494d233f43e1f199fbf4dfbcbdc01bee411847
Sumit Sahni <sumit.123sahni@gmail.com>
2021-06-02 00:19:01 +0530

    All file Added

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// const clacAge = function(birthYear)  {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+
+// clacAge(1997);
+
+// const clacAgeArrow = birthYear => {
+//     console.log(2040 - birthYear);
+//     console.log(this);
+// }
+
+// clacAgeArrow(1995);
+
+// const jonas = {
+//     name: 'johan',
+//     year: 1997,
+//     calAge: function () {
+//         console.log(this);
+//         console.log(2037 -this.year);
+//     }
+// }
+
+// jonas.calAge();
+
+// const matilda = {
+//     year: 2017
+// }
+
+// matilda.calAge = jonas.calAge
+// matilda.calAge();
+
+// const f = jonas.calAge;
+// console.log(f);
